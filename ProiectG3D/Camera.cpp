8223e8aed040c7baf7065e1ae090f6f59d9189ac
m1toi <alex.mitoi@student.unitbv.ro>
2023-11-29 14:08:35 +0200

    create constructor for Camera class, create Set and UpdateCameraVectors

---
+++ ProiectG3D/Camera.cpp
@@ -0,0 +1,41 @@
+#include "Camera.h"
+
+Camera::Camera(const int width, const int height, const glm::vec3& position)
+{
+	startPosition = position;
+	Set(width, height, position);
+
+}
+
+void Camera::Set(const int width, const int height, const glm::vec3& position)
+{
+	this->isPerspective = true;
+	this->yaw = YAW;
+	this->pitch = PITCH;
+
+	this->FoVy = FOV;
+	this->width = width;
+	this->height = height;
+	this->zNear = zNEAR;
+	this->zFar = zFAR;
+
+	this->worldUp = glm::vec3(0, 1, 0);
+	this->position = position;
+
+	lastX = width / 2.0f;
+	lastY = height / 2.0f;
+	bFirstMouseMove = true;
+
+	UpdateCameraVectors();
+}
+
+void Camera::UpdateCameraVectors()
+{
+	this->forward.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	this->forward.y = sin(glm::radians(pitch));
+	this->forward.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	this->forward = glm::normalize(this->forward);
+
+	right = glm::normalize(glm::cross(forward, worldUp));
+	up = glm::normalize(glm::cross(right, forward));
+}
--- ProiectG3D/Camera.h
+++ ProiectG3D/Camera.h
@@ -16,5 +16,41 @@
 
 class Camera
 {
+public:
+	Camera(const int width, const int height, const glm::vec3& position);
 
+	void Set(const int width, const int height, const glm::vec3& position);
+
+	void UpdateCameraVectors();
+
+private:
+	const float zNEAR = 0.1f;
+	const float zFAR = 500.f;
+	const float YAW = -90.0f;
+	const float PITCH = 0.0f;
+	const float FOV = 45.0f;
+	glm::vec3 startPosition;
+
+protected:
+	const float cameraSpeedFactor = 2.5f;
+	const float mouseSensitivity = 0.1f;
+	float zNear;
+	float zFar;
+	float FoVy;
+	int width;
+	int height;
+	bool isPerspective;
+
+	glm::vec3 position;
+	glm::vec3 forward;
+	glm::vec3 right;
+	glm::vec3 up;
+	glm::vec3 worldUp;
+
+	// Euler Angles 
+	float yaw;
+	float pitch;
+
+	bool bFirstMouseMove = true;
+	float lastX = 0.f, lastY = 0.f;
 };
