7a745f7a59f5d2a4d10d756f5b1f6a35bd108cb7
mattgorbava <imatei2003@gmail.com>
2023-12-04 15:21:24 +0200

    Update Shader.cpp

--- ProiectG3D/Shader.cpp
+++ ProiectG3D/Shader.cpp
@@ -1,1 +1,43 @@
 #include "Shader.h"
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+	Init(vertexPath, fragmentPath);
+}
+
+Shader::~Shader()
+{
+	glDeleteProgram(ID);
+}
+
+void Shader::Use() const
+{
+	glUseProgram(ID);
+}
+
+unsigned int Shader::GetID() const
+{
+	return ID;
+}
+
+void Shader::SetVec3(const std::string& name, const glm::vec3& value) const
+{
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void Shader::SetVec3(const std::string& name, float x, float y, float z) const
+{
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
+
+void Shader::SetFloat(const std::string& name, float val) const
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), val);
+}
+
+void Shader::SetMat4(const std::string& name, const glm::mat4& mat) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+
